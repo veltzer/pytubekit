2caccb986970cb58ed32283a37010dea06d40255
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-20 18:48:29 +0300

    removed all old pycontacts code

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pytubekit",
-    version="0.0.10",
+    version="0.0.1",
     packages=[
         'pytubekit',
         'pytubekit.endpoints',
@@ -40,6 +40,7 @@ setuptools.setup(
         'google-auth-oauthlib',
         'pytconf',
         'pylogconf',
+        'pyvardump',
     ],
     extras_require={
     },
