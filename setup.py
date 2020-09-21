5a638b404c03098401a48428c338b78ddd498d82
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-21 03:19:54 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pytubekit",
-    version="0.0.6",
+    version="0.0.7",
     packages=[
         'pytubekit',
         'pytubekit.endpoints',
