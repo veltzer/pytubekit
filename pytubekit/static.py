e3b6f1e07cf0bc20815762b3fafd4778e5123e34
Mark Veltzer <mark.veltzer@gmail.com>
2020-10-02 15:22:09 +0300

    new Makefile

---
+++ pytubekit/static.py
@@ -0,0 +1,4 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.10"
+DESCRIPTION = "Pytubekit will allow you to perform operations in your youtube account en masse"
+APP_NAME = "pytubekit"
