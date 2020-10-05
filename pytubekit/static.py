11d948aa7d98618dc93ee35dc3f639771daaf936
Mark Veltzer <mark.veltzer@gmail.com>
2020-10-05 14:01:49 +0300

    new version

--- pytubekit/static.py
+++ pytubekit/static.py
@@ -1,4 +1,4 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.10"
+VERSION_STR = "0.0.11"
 DESCRIPTION = "Pytubekit will allow you to perform operations in your youtube account en masse"
 APP_NAME = "pytubekit"
