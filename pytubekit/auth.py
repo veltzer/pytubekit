256995dab134d5f1e9babcc0410ef7fa1367c40c
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-14 14:30:43 +0200

    var dumping and more ops

--- pytubekit/auth.py
+++ pytubekit/auth.py
@@ -15,41 +15,62 @@ def scopes_md5(scopes: List[str]) -> str:
     return m.hexdigest()
 
 
+def ensure_folder(filename: str) -> None:
+    folder = os.path.dirname(filename)
+    if not os.path.exists(folder):
+        os.makedirs(os.path.dirname(filename))
+
+
 def get_credentials(
     app_name: str,
     scopes: List[str],
     logger: Logger,
+    host: str,
+    port: int,
+    authorization_prompt_message: str,
+    force: bool = False,
 ) -> Credentials:
+    """
+    The file token.pickle stores the user's access and refresh tokens, and is
+    created automatically when the authorization flow completes for the first
+    time.
+    """
     credentials = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     md5_of_scopes = scopes_md5(scopes)
     token_filename = os.path.expanduser(f"~/.config/google_tokens/token-{md5_of_scopes}.pickle")
+    logger.debug(f"reading credentials from [{token_filename}]")
+    if force:
+        if os.access(token_filename, os.R_OK):
+            os.unlink(token_filename)
     if os.access(token_filename, os.R_OK):
         with open(token_filename, "rb") as token_stream:
             credentials = pickle.load(token_stream)
-    # If there are no (valid) credentials available, let the user log in.
     if credentials is None or not credentials.valid:
         if credentials is not None:
             if credentials.expired and credentials.refresh_token:
                 logger.debug("refreshing credentials")
                 credentials.refresh(Request())
-            else:
-                pass
         else:
             client_secret = os.path.expanduser(f"~/.config/{app_name}/client_secret.json")
             if not os.path.isfile(client_secret):
-                raise IOError(f"missing client secret file {client_secret} for application {app_name}")
+                raise IOError(f"missing client secret file [{client_secret}] for application [{app_name}]")
+            logger.debug(f"creating credentials from client secret at {client_secret}")
             flow = InstalledAppFlow.from_client_secrets_file(
                 client_secret, scopes,
             )
-            credentials = flow.run_local_server(port=0)
-        # Save the token for the next run
-        logger.debug(f"creating a new token file {token_filename}")
+            credentials = flow.run_local_server(
+                host=host,
+                port=port,
+                authorization_prompt_message=authorization_prompt_message,
+            )
+        logger.debug(f"creating a new token file [{token_filename}]")
+        # there is a need to remove the old file if it exists since we chmod them so we can't overwrite them
         if os.access(token_filename, os.R_OK):
             os.unlink(token_filename)
+        ensure_folder(token_filename)
         with open(token_filename, "wb") as token_stream:
+            os.fchmod(token_stream.fileno(), 0o400)
             pickle.dump(credentials, token_stream)
-        os.chmod(token_filename, 0o400)
+    else:
+        logger.debug(f"have valid credentials in [{token_filename}]")
     return credentials
--- pytubekit/configs.py
+++ pytubekit/configs.py
@@ -1,3 +1,33 @@
 """
 All configurations for pytubekit
 """
+from pytconf import Config, ParamCreator
+
+
+class ConfigAuth(Config):
+    force = ParamCreator.create_bool(
+        help_string="Should we force creation of a new auth token",
+        default=False,
+    )
+    # parameters passed to run_local_server
+    host = ParamCreator.create_str(
+        help_string="The hostname for the local redirect server. This will be served over http, not https.",
+        default='localhost',
+    )
+    port = ParamCreator.create_int(
+        help_string="The port for the local redirect server.",
+        # default=8080,
+        default=0,
+    )
+    # noinspection PyProtectedMember
+    authorization_prompt_message = ParamCreator.create_str(
+        help_string="The message to display to tell the user to navigate to the authorization URL.",
+        # default=InstalledAppFlow._DEFAULT_AUTH_PROMPT_MESSAGE,
+        default="",
+    )
+
+
+class ConfigPlaylist(Config):
+    playlist = ParamCreator.create_str(
+        help_string="What playlist to use",
+    )
