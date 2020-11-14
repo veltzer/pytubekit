f10afd732d1efd487d80ffd28c90de19d8612f95
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-14 18:51:46 +0200

    doing pagination

--- pytubekit/main.py
+++ pytubekit/main.py
@@ -6,7 +6,7 @@ import os
 
 import googleapiclient.discovery
 import pylogconf.core
-import pyvardump
+# import pyvardump
 from pytconf import register_main, config_arg_parse_and_launch, register_endpoint
 
 from pytubekit import LOGGER_NAME
@@ -23,6 +23,8 @@ SCOPES = [
     "https://www.googleapis.com/auth/youtube.force-ssl",
     "https://www.googleapis.com/auth/youtube.readonly",
 ]
+NEXT_PAGE_TOKEN = "nextPageToken"
+PAGE_TOKEN = "pageToken"
 
 
 def get_youtube():
@@ -44,30 +46,45 @@ def get_youtube():
     return youtube
 
 
+def create_list_request(youtube, next_page_token):
+    # pylint: disable=no-member
+    kwargs = {
+        # part="snippet,contentDetails",
+        # part="contentDetails",
+        # part="status",
+        "part": "snippet",
+        "maxResults": 25,
+        "mine": True,
+    }
+    if next_page_token:
+        kwargs[PAGE_TOKEN] = next_page_token
+    return youtube.playlists().list(**kwargs)
+
+
 @register_endpoint(
     description="Show all playlists in your youtube account",
 )
 def playlists() -> None:
     youtube = get_youtube()
-    # pylint: disable=no-member
-    request = youtube.playlists().list(
-        # part="snippet,contentDetails",
-        # part="contentDetails",
-        part="status",
-        maxResults=5,
-        mine=True,
-    )
-    response = request.execute()
-    pyvardump.dump_pprint(response)
-    import sys
-    sys.exit(1)
 
-    for x in response["items"]:
-        pyvardump.dump_pprint(x["snippet"])
-        import sys
-        sys.exit(1)
-        print(x["snippet"]["title"])
-        print(x["snippet"]["description"])
+    next_page_token = None
+    while True:
+        request = create_list_request(youtube, next_page_token)
+        response = request.execute()
+        # pyvardump.dump_pprint(response)
+        # import sys
+        # sys.exit(1)
+
+        for x in response["items"]:
+            # pyvardump.dump_pprint(x["snippet"])
+            # import sys
+            # sys.exit(1)
+            print(x["snippet"]["title"])
+            # print(x["snippet"]["description"])
+        if NEXT_PAGE_TOKEN in response:
+            next_page_token = response[NEXT_PAGE_TOKEN]
+        else:
+            break
 
 
 @register_endpoint(
