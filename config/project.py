61ec4f1f5c25b835c4c764e79918cdc1ef7a8144
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 19:49:29 +0300

    fix config

--- config/project.py
+++ config/project.py
@@ -44,7 +44,7 @@ project_classifiers = [
 ]
 
 project_data_files = []
-# project_data_files.append(templar.utils.hlp_files_under('/usr/bin', 'src/*'))
+# project_data_files.append(templar.utils.hlp_files_under("/usr/bin", "src/*"))
 
 project_copyright_years = ", ".join(
     map(str, range(int(project_year_started), datetime.datetime.now().year + 1))
@@ -57,13 +57,13 @@ else:
     )
 
 project_google_analytics_snipplet = """<script type="text/javascript">
-(function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
+(function(i,s,o,g,r,a,m){{i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){{
 (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-}})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+}})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
 
-ga('create', '{0}', 'auto');
-ga('send', 'pageview');
+ga("create", "{0}", "auto");
+ga("send", "pageview");
 
 </script>""".format(
     project_google_analytics_tracking_id
