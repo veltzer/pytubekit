13ac1869dea9b4e8cb97c85d9e5f190b3a379c5b
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:44 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,7 +1,3 @@
-import config.project
-
-package_name = config.project.project_name
-
 console_scripts = [
     "pytubekit=pytubekit.main:main",
 ]
@@ -30,8 +26,3 @@ test_requires = [
     "flake8",
     "pymakehelper",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
