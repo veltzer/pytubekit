49f17068ebfaef0204b351ab41d42d21838f5ba9
Mark Veltzer <mark.veltzer@gmail.com>
2022-08-15 16:48:31 +0300

    new version

--- pytubekit/static.py
+++ pytubekit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.20"
+VERSION_STR = "0.0.21"
 DESCRIPTION = "Extra youtube operations"
 APP_NAME = "pytubekit"
 LOGGER_NAME = "pytubekit"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pytubekit",
-    version="0.0.20",
+    version="0.0.21",
     packages=[
         'pytubekit',
     ],
