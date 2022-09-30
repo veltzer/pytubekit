a4fde498c9000349b7d096ec4ae474954bc82c1a
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-01 02:43:58 +0300

    new version

--- pytubekit/static.py
+++ pytubekit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.24"
+VERSION_STR = "0.0.25"
 DESCRIPTION = "Extra youtube operations"
 APP_NAME = "pytubekit"
 LOGGER_NAME = "pytubekit"
