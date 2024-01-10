4b7ab6fce515f3407b30d3544086ca184234238b
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 18:18:19 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -4,10 +4,12 @@ from typing import List
 console_scripts: List[str] = [
     "pytubekit=pytubekit.main:main",
 ]
-config_requires: List[str] = []
 dev_requires: List[str] = [
     "pypitools",
     "black",
+]
+config_requires: List[str] = [
+    "pyclassifiers",
 ]
 install_requires: List[str] = [
     "google-api-python-client",
@@ -19,7 +21,6 @@ install_requires: List[str] = [
     "browsercookie",
 ]
 make_requires: List[str] = [
-    "pyclassifiers",
     "pydmt",
     "pymakehelper",
 ]
