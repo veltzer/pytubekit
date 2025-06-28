47250ed1a77e89a1fb9a0d7968ad5af7c7124840
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:25 +0300

    more

--- config/python.py
+++ config/python.py
@@ -1,8 +1,8 @@
 """ python deps for this project """
 
-console_scripts: list[str] = [
-    "pytubekit=pytubekit.main:main",
-]
+scripts: dict[str, str] = {
+    "pytubekit": "pytubekit.main:main",
+}
 
 config_requires: list[str] = [
     "pyclassifiers",
