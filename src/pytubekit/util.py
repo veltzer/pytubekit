4f31508daa9376c8dccd08b33ab2e4574c34cd59
Mark Veltzer <mark.veltzer@gmail.com>
2026-01-31 18:22:57 +0200

    no commit message given

--- src/pytubekit/util.py
+++ src/pytubekit/util.py
@@ -159,3 +159,8 @@ def get_video_ids_from_playlist_names(youtube, names: list[str]) -> set[str]:
     playlist_ids = get_playlist_ids_from_names(youtube, names)
     items = get_all_items_from_playlist_ids(youtube, playlist_ids)
     return {item["snippet"]["resourceId"]["videoId"] for item in items}
+
+
+def get_items_from_playlist_names(youtube, names: list[str]):
+    playlist_ids = get_playlist_ids_from_names(youtube, names)
+    return get_all_items_from_playlist_ids(youtube, playlist_ids)
